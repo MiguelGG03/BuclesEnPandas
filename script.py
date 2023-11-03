23d5329ebc51ab483a181301f5d8d68ff2c543c1
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-03 19:22:53 +0100

    acabado

--- script.py
+++ script.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 22})
 
 # Techniques
-def forloop(df):
+def forloop(df,col1,col2):
     total = []
     for index in range(len(df)):
-        total.append(df['col1'].iloc[index]
-                    + df['col2'].iloc[index])
+        total.append(df[col1].iloc[index]
+                    + df[col2].iloc[index])
     return total
 
 def itertuples(df):
@@ -19,25 +19,25 @@ def itertuples(df):
         total.append(row[1] + row[2])
     return total
 
-def iterrows(df):
+def iterrows(df,col1,col2):
     total = []
     for index, row in df.iterrows():
-        total.append(row['col1']
-                   + row['col2'])
+        total.append(row[col1]
+                   + row[col2])
     return total
 
-def apply(df):
-    return df.apply(lambda row: row['col1']
-                              + row['col2'], axis=1).to_list()
+def apply(df,col1,col2):
+    return df.apply(lambda row: row[col1]
+                              + row[col2], axis=1).to_list()
 
-def comprehension(df):
-    return [src + dst for src, dst in zip(df['col1'], df['col2'])]
+def comprehension(df,col1,col2):
+    return [src + dst for src, dst in zip(df[col1], df[col2])]
 
-def pd_vectorize(df):
-    return (df['col1'] + df['col2']).to_list()
+def pd_vectorize(df,col1,col2):
+    return (df[col1] + df[col2]).to_list()
 
-def np_vectorize(df):
-    return (df['col1'].to_numpy() + df['col2'].to_numpy()).tolist()
+def np_vectorize(df,col1,col2):
+    return (df[col1].to_numpy() + df[col2].to_numpy()).tolist()
 
 
 
@@ -50,7 +50,10 @@ def main():
           "existentes: src_bytes y  dst_bytes. Veamos diferentes métodos para calcular esta nueva\n"
           "característica.")
 
-    eleccion = input("Que metodo deseas usar?\n(1) Iterrows ❌\n(2) Bucle for con .iloc o .loc ❌\n(3) Metodo Apply ❌\n(4) Itertuplica ❌\n(5) Compresion de listas ❌ \n(6) Vectorización de pandas (x1500 mas rapido)✅\n(7) Vectorización NumPy (x1900 mas rapido)✅✅\n(8) Perfplot (Bonus)✅✅📈\n>>> ")
+    print(df.info())
+    _cl1= df["dst_host_same_srv_rate"]
+    _cl2= df["dst_host_diff_srv_rate"]
+    eleccion = input("Que metodo deseas usar?\n(6) Vectorización de pandas (x1500 mas rapido)✅\n(7) Vectorización NumPy (x1900 mas rapido)✅✅\n(8) Perfplot (Bonus)✅✅📈\n>>> ")
     if eleccion == "8":
         # Perfplot
         functions = [iterrows, forloop, apply, itertuples, comprehension, pd_vectorize, np_vectorize]
@@ -64,13 +67,23 @@ def main():
             n_range=[2**n for n in range(20)],
             xlabel='Number of rows',
         )
-
+    
+    elif eleccion=="6":
+        # Vectorización de pandas
+        print("Vectorización de pandas")
+        resultado = pd_vectorize(df,_cl1,_cl2)
         plt.figure(figsize=(20,12))
-        out.show()
-    elif eleccion=="1":
-        # Iterrows
-        print("Iterrows") 
-        iterrows(df)
+        plt.plot(resultado)
+        plt.show()
+
+        
+    elif eleccion=="7":
+        # Vectorización NumPy
+        print("Vectorización NumPy")
+        resultado = np_vectorize(df,_cl1,_cl2)
+        plt.figure(figsize=(20,12))
+        plt.plot(resultado)
+        plt.show()
 
 if __name__=="__main__":
     main()
